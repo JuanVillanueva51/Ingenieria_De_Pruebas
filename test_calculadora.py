7928399632ea65eca4607da3ac900384d5e68417
JuanVillanueva51 <juan.villanueva51@uabc.edu.mx>
2026-09-11 22:20:56 -0700

    teste de multiply

--- test_calculadora.py
+++ test_calculadora.py
@@ -4,3 +4,6 @@ def test_add():
  #Hola, este es un comentario
     calc = calculadora.Calculadora()
     assert calc.add(2, 3) == 5
+def test_multiply():
+    calc = calculadora.Calculadora()
+    assert calc.multiply(4, 5) == 20
